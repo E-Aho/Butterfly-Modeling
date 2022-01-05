523a82760aca7f710813af8540fa67d31e6c98f0
e-aho <erik.aho24@gmail.com>
2022-01-05 12:17:35 +0000

    WIP

--- main.py
+++ main.py
@@ -1,9 +1,82 @@
-import pandas as pd
+import math
+from typing import List
+
 import numpy as np
+import pandas as pd
+
+
+def get_inputs(filename: str = "data/ButterflyFeatures.csv") -> pd.DataFrame:
+
+    output_df = pd.read_csv(filename)
+    return output_df
+
+
+def compute_jmi(
+        label: pd.DataFrame,
+        features: pd.DataFrame,
+        feature_count: int = math.inf,
+        produce_plots: bool = False
+) -> [List, List]:
+    """Performs feature selection based on JMI (Joint Mutual Information),
+     and returns a list of the ordered features and the amount of information they add.
+
+     :param label: the target variable (continuous) to compute information against
+     :param features: the features to use in feature selection
+     :param feature_count: the limit of features to compute. Defaults to computing all features
+     :param produce_plots: whether to print and save plots. Defaults to False
+     :returns:  a list of the ordered features, and a list of the amount of information they each add"""
+
+    information = 0
+    selected_features = []
+    information_gains = []
+    features_map = {col: i for col, i in sorted(enumerate(list(features.columns)))}
+    remaining_features = list(features.columns)
+
+    is_discrete = get_discrete_dict(features)
+
+    first_info, first_feature = get_first_mi(Y=label, X=features.values, is_discrete=is_discrete)
+
+    while len(selected_features) <= min(feature_count, len(features)):
+        pass
+
+    return selected_features, information_gains
+
+
+def get_first_mi(X, Y, is_discrete):
+    pass
+
+def get_mi(X, Y, is_discrete):
+    if is_discrete:
+        return
+    else:
+        return
+
+def get_discrete_dict(features):
+    """Returns a dictionary that tracks if a feature is discrete or continuous"""
+    #     NB: For this project, the only discrete feature is binary, so we only check for binary variables.
+    d = {}
+    for col in features.columns:
+        if set(features[col]) == {0, 1} or {True, False}:
+            d[col] = True
+        else:
+            d[col] = False
+    return d
 
 
 def main_entrypoint():
     """Main entrypoint for the code used to analyse factors relating to species richness of butterflies in nations"""
+    input_df = get_inputs()
+    input_df.sort_index();
+
+    label_col = "logSpeciesDensity"
+    non_feature_cols = [label_col, "Country", "SpeciesDensity"]
+
+    label = input_df[label_col]
+    features = input_df[[col for col in input_df.columns if col not in non_feature_cols]]
+
+    compute_jmi(label, features)
+
+
 
 
 if __name__ == "__main__":
